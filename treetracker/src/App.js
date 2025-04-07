f42fb3e7a736c72d1f45297088ab9747fe1f4143
Logan Oram <logan.oram@keyin.com>
2025-04-07 09:31:48 -0230

    app.js

--- treetracker/src/App.js
+++ treetracker/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import TreeForm from "./components/TreeForm";
+import TreeList from "./components/TreeList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<TreeForm />} />
+        <Route path="/previous" element={<TreeList />} />
+      </Routes>
+    </Router>
   );
 }
 
